2f2d21530c927d71692b6110e4d33fd2568b7ad6
asad uzzaman <asaduzzaman@asads-MacBook-Pro.local>
2015-01-25 20:02:55 -0500

    added a text file

---
+++ JavaClassNote/src/B.java
@@ -0,0 +1,17 @@
+
+public class B 
+{
+	int c = 10;
+	
+	void m1()
+	{
+		String s = "string S value";
+		System.out.println(s);
+		System.out.println("hi");
+		
+	}
+
+	
+	
+
+}
---
+++ JavaClassNote/src/Note.java
@@ -0,0 +1,46 @@
+
+public class Note 
+{
+/*	
+	Data type: 
+	Primitive Data type = (byte, short, int, long, float, double, boolean, char)
+	References data type = (tring, Scanner, Random, Die, int[], String[], etc.)
+	
+	Return Type 
+	When a data type used for a method it's call Return Type:
+	String, Scanner, Random, Die, int[], String[], etc.
+	
+	Static variable and method can be called with class name in other class:
+	Class P
+	{
+		Static int a = 5;
+		Static void abc()
+		{
+			Syso("hi");
+		}
+	}
+	
+	Class Q
+	{
+		void xyz()
+		{
+			P.a;
+			P.abc();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+*/
+	
+	
+
+}
